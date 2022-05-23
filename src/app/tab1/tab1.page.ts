06ae448049d6ed5d54965c701348ea0c99328eef
Jeremy210321 <jeremy.leon@epn.edu.ec>
2022-05-23 16:37:40 -0500

    Cambios

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
 
 @Component({
   selector: 'app-tab1',
@@ -7,21 +6,5 @@ import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-  code:any;
-
-  constructor(private barcodeScanner:BarcodeScanner) {}
-
-  ngOnInit() {
-
-  }
-  scan(){
-    this.barcodeScanner.scan().then(barcodeData => {
-      console.log("Barcode data: ", barcodeData);
-      this.code=barcodeData.text;
-      console.log("Barcode data Text: ", this.code);
-      
-    }).catch(error=>{
-        console.log("Error: ", error)
-    })
-  }
+  constructor() {}
 }
